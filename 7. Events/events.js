13e6225d924e7592fdc2a6602f7f8e3e164ee93f
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-04-17 07:31:53 +0530

    learn Event handeling in JS

--- 7. Events/events.js
+++ 7. Events/events.js
@@ -1,11 +1,8 @@
-// let btn1 = document.querySelector("#btn1");
+let btn1 = document.querySelector("#btn1");
 
-// btn1.onclick = () => {
-//     console.log("btn1 was clicked");
-//     let a = 25;
-//     a++;
-//     console.log(a); //26
-// };
+btn1.onclick = () => {
+    console.log("btn1 was clicked");
+}
 
 // let div = document.querySelector("div");
 // div.onmouseover = () => {
